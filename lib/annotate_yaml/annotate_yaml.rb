e20019e0cd75ce0475d639ff0aab829999a32913
Benjamin Roth <apnea.diving.deep@gmail.com>
2014-08-31 23:11:13 +0200

    refactored method to objects

--- lib/annotate_yaml/annotate_yaml.rb
+++ lib/annotate_yaml/annotate_yaml.rb
@@ -1,57 +1,113 @@
 module AnnotateYaml
   require 'yaml'
 
-  class << self
-    def annotate_locales
+  DEFAULT_YML_LOOKUP = 'config/locales/*.yml'
 
-      files = Dir.glob("config/locales/*.yml")
-      files.each do |file|
-        complete_yaml_file = File.open(file).read
-        # Remove the references
-        complete_yaml_file_without_references = complete_yaml_file.gsub(/<<: \*.*/, '')
-        yaml_hash = YAML::load(complete_yaml_file_without_references)
-        array_of_hashes_of_values_with_keys = []
-        yaml_navigation(yaml_hash, [], array_of_hashes_of_values_with_keys)
+  class YamlHashExplorer
+    attr_reader :result
 
-        string_result = ''
-
-        text = File.open(file).read
-        text.gsub!(/\r\n?/, "\n")
-        current_hash = array_of_hashes_of_values_with_keys.shift
-        text.each_line do |line|
-          begin
-            line_hash = YAML::load line
-          rescue Exception => e
-            string_result += line
-            next
-          end
-          if line_hash === false
-            string_result += line
-          else
-            line_key, line_value = line_hash.first
-            key, value = current_hash.first
-            if line_value == key
-              string_result += (line.end_with?(" \# #{value}\n") ? line : line.gsub("\n", '') + " \# #{value}\n")
-              current_hash = array_of_hashes_of_values_with_keys.shift
-            else
-              string_result += line
-            end
-          end
-
-          File.open(file, "w+") do |f|
-            f.write(string_result)
-          end
-        end
-      end
+    def initialize(hash)
+      @result = []
+      yaml_navigation(hash, [])
     end
 
-    def yaml_navigation(yaml_hash, keys_array_origin = [], result = [])
+    private
+
+    def yaml_navigation(yaml_hash, keys_array_origin)
       yaml_hash.each do |key, value|
         keys_array_updated = keys_array_origin.dup
         keys_array_updated.push(key)
-        result.push({ value => keys_array_updated.join('.') }) unless value.nil? || value.is_a?(Hash) || value.is_a?(Array)
-        yaml_navigation value, keys_array_updated, result if value.is_a?(Hash)
+        result.push({ value => keys_array_updated.join('.') }) if desired_terminatory_value?(value)
+
+        yaml_navigation(value, keys_array_updated) if value.is_a?(Hash)
       end
+    end
+
+    def desired_terminatory_value?(value)
+      !(value.nil? || value.is_a?(Hash) || value.is_a?(Array))
+    end
+  end
+
+  class FileHandler
+
+    attr_reader :file_name, :result
+
+    def initialize(file_name)
+      @file_name = file_name
+      @result = ""
+    end
+
+    def call
+      create_annotated_text
+      rewrite_file
+    end
+
+    private
+
+    def create_annotated_text
+      adapted_file_content.each_line do |line|
+        annotate_line(line)
+      end
+    end
+
+    def annotate_line(line)
+      if line_hash = YAML::load(line)
+        _, line_value = line_hash.first
+
+        if line_value.nil?
+          result << line
+        else
+          key, value = current_hash.first
+          if line_value == key
+            result << annotation_for_line(line, value)
+          else
+            result << line
+          end
+        end
+      else
+        result << line
+      end
+    # did exist before refactoring, not sure why
+    # rescue Exception
+    #   result << line
+    end
+
+    def annotation_for_line(line, value)
+      (line.end_with?(" \# #{value}\n") ? line : line.gsub("\n", '') + " \# #{value}\n")
+    end
+
+    def rewrite_file
+      File.open(file_name, "w+") do |f|
+        f.write(result)
+      end
+    end
+
+    def adapted_file_content
+      File.open(file_name).read.gsub(/\r\n?/, "\n")
+    end
+
+    def hash_content
+      YAML.load_file(file_name)
+    end
+
+    def array_of_hashes_of_values_with_keys
+      @array_of_hashes_of_values_with_keys ||= YamlHashExplorer.new(hash_content).result
+    end
+
+    def current_hash
+      array_of_hashes_of_values_with_keys.shift
+    end
+  end
+
+  class << self
+    def annotate_locales
+      file_names.each do |file_name|
+        FileHandler.new(file_name).call
+      end
+    end
+
+    def file_names
+      Dir.glob(DEFAULT_YML_LOOKUP)
     end
   end
 
